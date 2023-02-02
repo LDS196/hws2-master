f5d50e70da7b7c5323e2c754f0a47dbda3e4e615
LDS196 <lihachevd87@gmail.com>
2023-02-02 19:32:45 +0400

    hw#9 update

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -64,11 +64,11 @@ function Clock() {
     }
 
 
-    const stringDate = `${day}:${mounth}:${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${day}.${mounth}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = DAYS[date.getDay()] || <br/> // пишут студенты
-    const stringMonth = MONTHS[date.getDate()] || <br/> // пишут студенты
+    const stringMonth = MONTHS[date.getDate() - 1] || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
