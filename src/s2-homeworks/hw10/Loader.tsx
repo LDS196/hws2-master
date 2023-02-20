3ea1e16da3e3dedd5e6e452958d0787ec22a2166
LDS196 <lihachevd87@gmail.com>
2023-02-20 17:34:04 +0400

    hw#11 update

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -4,7 +4,6 @@ export const Loader = () => {
     return(
         <div className={s.loader}>
             <img src={loader} alt=""/>
-
             </div>
     )
 }
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import {red} from "@mui/material/colors";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
+
             sx={{ // стили для слайдера // пишет студент
-                
+                 mr:3,
+                 ml:3,
+                maxWidth: 300
+
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
