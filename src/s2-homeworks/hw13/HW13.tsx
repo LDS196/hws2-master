2747eded2025a4d01ae3dad54fd81d24c95a1d8b
LDS196 <lihachevd87@gmail.com>
2023-03-03 21:13:40 +0400

    hw#13 update

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -21,7 +21,7 @@ const HW13 = () => {
     const [info, setInfo] = useState('')
     const [image, setImage] = useState('')
 
-    const send = (x?: boolean | null) => () => {
+    const send = (x?: boolean | null | undefined) => () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
@@ -37,7 +37,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок)\nкод 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок)код 200 - обычно означает что скорее всего всё ок)')
                 setInfo('')
 
 
@@ -46,19 +46,19 @@ const HW13 = () => {
                 if(e.response.status===500){
                     setCode('Ошибка 500!')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере\n ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
                     setInfo('')
                 }
                 if(e.response.status===400){
                     setCode('Ошибка 400!')
                     setImage(error400)
-                    setText('Ты не отправил success в body вообще!\n ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setText('Ты не отправил success в body вообще!ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                     setInfo('')
                 }
                 if(e.response.status===0){
                     setCode('Error!')
                     setImage(errorUnknown)
-                    setText('Network Error\n + AxiosError')
+                    setText('Network Error AxiosError')
                     setInfo('')
                 }
             })
